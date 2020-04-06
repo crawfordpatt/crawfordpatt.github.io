104e6f88ae335b331f982069b815ce88c4936866
Crawfordpatt <crawfordpatten2@gmail.com>
2020-04-06 10:16:11 -0700

    Oh boy

--- covid-19/covid.js
+++ covid-19/covid.js
@@ -1,4 +1,4 @@
-const api_url = "http://corona-api.com/countries/US";
+/*const api_url = "http://corona-api.com/countries/US";
 async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
@@ -12,5 +12,23 @@ async function getData() {
     document.getElementById("confirmedDiv").innerHTML = confirmed;
     document.getElementById("casesPerMillionDiv").innerHTML = casesPerMillion;
 }
+
+getData();
+*/
+const api_url = "http://corona-api.com/countries/US";
+    async function getData() {
+        const response = await fetch(api_url);
+        const data = await response.json();
+        console.log(data);
+        const latestData = data.data.latest_data;
+        console.log(latestData);
+        const deaths = latestData.deaths;
+        const confirmed = latestData.confirmed;
+        const casesPerMillion = latestData.calculated.cases_per_million_population;
+
+        document.getElementById("deathsDiv").innerHTML = "Total Deaths (U.S): " + deaths;
+        document.getElementById("confirmedDiv").innerHTML = "Total US Confirmed Cases (U.S): " + confirmed;
+        document.getElementById("casesPerMillionDiv").innerHTML = "Cases per Million (U.S): " + casesPerMillion;
+    }
 
 getData();

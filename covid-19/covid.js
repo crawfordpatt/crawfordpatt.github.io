bf6dc6422f7a4c5cc32fed1690608e8a5ffdf4de
Crawfordpatt <crawfordpatten2@gmail.com>
2020-04-10 10:51:44 -0700

    Background color

--- covid-19/covid.js
+++ covid-19/covid.js
@@ -1,34 +1,17 @@
-/*const api_url = "http://corona-api.com/countries/US";
+const api_url = "http://corona-api.com/countries/US";
 async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
+    console.log(data);
+    const latestData = data.data.latest_data;
+    console.log(latestData);
+    const deaths = latestData.deaths;
+    const confirmed = latestData.confirmed;
+    const casesPerMillion = latestData.calculated.cases_per_million_population;
 
-    let deaths = data.latestData.deaths;
-    let confirmed = latestData.confirmed;
-    let casesPerMillion = latestData.calculated.cases_per_million_population;
-    //const bruh = "Greetings";
-
-    document.getElementById("deathsDiv").innerHTML = "Hi";
-    document.getElementById("confirmedDiv").innerHTML = confirmed;
-    document.getElementById("casesPerMillionDiv").innerHTML = casesPerMillion;
+    document.getElementById("deathsDiv").innerHTML = "Total Deaths (U.S): " + deaths;
+    document.getElementById("confirmedDiv").innerHTML = "Total US Confirmed Cases (U.S): " + confirmed;
+    document.getElementById("casesPerMillionDiv").innerHTML = "Cases per Million (U.S): " + casesPerMillion;
 }
-
-getData();
-*/
-const api_url = "http://corona-api.com/countries/US";
-    async function getData() {
-        const response = await fetch(api_url);
-        const data = await response.json();
-        console.log(data);
-        const latestData = data.data.latest_data;
-        console.log(latestData);
-        const deaths = latestData.deaths;
-        const confirmed = latestData.confirmed;
-        const casesPerMillion = latestData.calculated.cases_per_million_population;
-
-        document.getElementById("deathsDiv").innerHTML = "Total Deaths (U.S): " + deaths;
-        document.getElementById("confirmedDiv").innerHTML = "Total US Confirmed Cases (U.S): " + confirmed;
-        document.getElementById("casesPerMillionDiv").innerHTML = "Cases per Million (U.S): " + casesPerMillion;
-    }
 
 getData();
